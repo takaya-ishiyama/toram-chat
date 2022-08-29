647b0fb49cf53b0afc285a04ca605570f94897f1
takaya <takaya_ishiyama0810@yahoo.co.jp>
2022-08-29 11:19:50 +0900

    ...

--- toram/toram/config/settings.py
+++ toram/toram/config/settings.py
@@ -129,7 +129,6 @@ USE_L10N = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-# 153.121.57.137は本番環境ip
 ALLOWED_HOSTS = ['127.0.0.1','153.121.57.137','localhost']
 
 STATIC_URL = '/static/'
